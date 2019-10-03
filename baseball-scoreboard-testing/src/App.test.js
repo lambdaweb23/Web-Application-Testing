54bac3e03ae4a668b2c2aaa03a2b8c9cdf04ddae
Kenna <kennaalastair@gmail.com>
2019-10-03 15:41:15 -0700

    6/6 unit tests passed

--- baseball-scoreboard-testing/src/App.test.js
+++ baseball-scoreboard-testing/src/App.test.js
@@ -3,7 +3,6 @@ import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 import Dashboard from './components/Dashboard';
-import { ballCount, strikeCount, setBallCount, setStrikeCount } from './App';
 
 it('renders without crashing', () => {
   render(<App />);
@@ -23,6 +22,30 @@ describe('<Dashboard />', () => {
     const { getByText } = render(<Dashboard strikeCount={() => clicked = true} />);
     const strikeButton = getByText(/strike/i);
     fireEvent.click(strikeButton);
+    expect(clicked).toBe(true);
+  });
+
+  it('ball exists and onclick', () => {
+    let clicked = false;
+    const { getByText } = render(<Dashboard ballCount={() => clicked = true} />);
+    const ballButton = getByText(/ball/i);
+    fireEvent.click(ballButton);
+    expect(clicked).toBe(true);
+  });
+
+  it('foul exists and onclick', () => {
+    let clicked = false;
+    const { getByText } = render(<Dashboard foulCount={() => clicked = true} />);
+    const foulButton = getByText(/foul/i);
+    fireEvent.click(foulButton);
+    expect(clicked).toBe(true);
+  });
+
+  it('hit exists and onclick', () => {
+    let clicked = false;
+    const { getByText } = render(<Dashboard hitCount={() => clicked = true} />);
+    const hitButton = getByText(/hit/i);
+    fireEvent.click(hitButton);
     expect(clicked).toBe(true);
   });
 });
