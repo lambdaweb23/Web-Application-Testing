f70f7e1762b247785d9f2bec456e400a1a694e87
Kenna <kennaalastair@gmail.com>
2019-10-03 13:13:55 -0700

    dashboard structure

---
+++ baseball-scoreboard-testing/src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <div className="buttons">
+                <button className="ball-button">
+                    Ball
+                </button>
+                <button className="strike-button">
+                    Strike
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
